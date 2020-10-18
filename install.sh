0cb896deadc820333ec189a3c190ca1890cbe3be
Charlie Moog <moogcharlie@gmail.com>
2020-10-18 00:40:39 -0500

    Install vundle and vim plugins

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@
 cd "$(dirname "$0")"
 
 info() {
-  echo "$@"
+  echo "-- $@"
 }
 
 warn() {
-  echo "$(tput setaf 1)$@$(tput sgr0)"
+  echo "$(tput setaf 1)-- $@$(tput sgr0)"
 }
 
 install() {
@@ -17,9 +17,22 @@ install() {
   ln -sf $src $dst
 }
 
+gitstall() {
+  if [ -d "$2" ]; then
+    rm -rf "$2"
+  fi
+  git clone "$1" "$2"
+  info "cloning $1 to $2"
+}
+
 warn "Linking files"
 
 install config.fish ~/.config/fish/config.fish
 install fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish
 install .gitconfig
 install .vimrc
+
+gitstall https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+info "Installing vim plugins"
+vim +PluginInstall +qall
