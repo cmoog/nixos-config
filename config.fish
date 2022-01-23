d5675c11b74ef82c1f3793197a425adc9ec84329
Charlie Moog <moogcharlie@gmail.com>
2022-01-23 14:14:31 -0600

    clean

--- config.fish
+++ config.fish
@@ -1,6 +1,7 @@
+set fish_greeting ""
 set --export EDITOR vim
 
-# Golang
+# Go toolchain configuration
 set --export GOPATH ~/go
 set --export GO111MODULE on
 
@@ -15,19 +16,16 @@ if [ (uname) = "Darwin" ]
   # use GNU/Linux utils on macOS
   set --local brew_prefix (brew --prefix)
   fish_add_path $brew_prefix/opt/coreutils/libexec/gnubin \
-    $brew_prefix/opt/findutils/libexec/gnubin \
     $brew_prefix/opt/make/libexec/gnubin \
     $brew_prefix/opt/gnu-tar/libexec/gnubin \
     $brew_prefix/opt/gnu-sed/libexec/gnubin \
-    $brew_prefix/opt/gawk/libexec/gnubin \
     $brew_prefix/opt/curl/bin
+
+  # alias Tailscale CLI
   alias tailscale /Applications/Tailscale.app/Contents/MacOS/Tailscale
+
+  # use Secretive for SSH authentication with the secure enclave
   set --export SSH_AUTH_SOCK ~/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh
-  source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc"
-else
-  fish_add_path \
-    /home/linuxbrew/.linuxbrew/bin \
-    /usr/local/gcloud/google-cloud-sdk/bin
 end
 
 # convenience abbreviations
@@ -35,13 +33,16 @@ abbr --add --global g 'git'
 abbr --add --global kube 'kubectl'
 abbr --add --global kub 'kubectl'
 
+# alias to quickly enter a nix-shell during dev
+abbr --add --global ns 'nix-shell'
+
 # prefer "exa" to "ls" when available
 if type -q exa
   abbr --add --global ls 'exa'
   abbr --add --global lss 'ls'
 end
 
-# lazygit and lazydocker
+# TUIs for git and docker
 abbr --add --global lg 'lazygit'
 abbr --add --global ld 'lazydocker'
 
